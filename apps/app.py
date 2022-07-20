4191f03ac56af787dadf614d5d39993d52c248ee
OhnoHaruki <haruki20010219@gmail.com>
2022-07-20 17:40:58 +0900

    sixes commit 7/20

--- apps/app.py
+++ apps/app.py
@@ -1,14 +1,24 @@
+from pathlib import Path
+
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 
 # create_app関数を使用する
 def create_app():
     # Flaskインスタンスを作成
     app = Flask(__name__)
-
+    # アプリのコンフィグ設定
+    # SQLalchemyとアプリを連携する
+    db.init_app(app)
+    # Migrateとアプリを設定する
+    Migrate(app, db)
     # crudパッケージからviewsをインポートする
     from apps.crud import views as crud_views
 
-    # register_blueprintsを使ってviewsのcrudアプリをアプリへ登録
+    # register_blueprintsを使ってviewsのcrudをアプリへ登録
     app.register_blueprint(crud_views.crud, url_prefix="/crud")
     return app
